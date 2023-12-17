a819ccf57e868aea500e29d66a68f88d4cb61d7c
AlexPrograms <olexiy2004@gmail.com>
2023-12-17 18:26:24 +0100

    Initial MVC set up | No DB

---
+++ BlackPieShop/Models/ICategoryRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace BlackPieShop.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
---
+++ BlackPieShop/ViewModels/PieListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BlackPieShop.Models;
+
+namespace BlackPieShop.ViewModels
+{
+    public class PieListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; }
+        public string? CurrentCategory { get; }
+
+        public PieListViewModel(IEnumerable<Pie> pies, string? currentCategory)
+        {
+            Pies = pies;
+            CurrentCategory = currentCategory;
+        }
+    }
+}
