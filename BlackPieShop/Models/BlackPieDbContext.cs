ad11b8f82a1111ab0b573f0ee18ab394199c9e3b
AlexPrograms <olexiy2004@gmail.com>
2023-12-17 20:11:11 +0100

    Added DbContext and configured connection

---
+++ BlackPieShop/Models/BlackPieDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlackPieShop.Models
+{
+    public class BlackPieDbContext : DbContext
+    {
+        public BlackPieDbContext(DbContextOptions<BlackPieDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
+
+    }
+}
--- BlackPieShop/Program.cs
+++ BlackPieShop/Program.cs
@@ -1,10 +1,16 @@
 using BlackPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+builder.Services.AddDbContext<BlackPieDbContext>(options => {
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:BlackPieDbContextConnection"]);
+    });
 
 builder.Services.AddControllersWithViews();
 
